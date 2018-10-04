b400596d2a0281dab46213f3f4a77ee4401b5563
Brian Amesbury <bamesbury@sofi.org>
2018-10-04 06:20:03 -0600

    annotate supress unused

--- shared-preference-live-data/src/main/java/com/moonlitdoor/shared/preference/live/data/Ext.kt
+++ shared-preference-live-data/src/main/java/com/moonlitdoor/shared/preference/live/data/Ext.kt
@@ -2,16 +2,23 @@ package com.moonlitdoor.shared.preference.live.data
 
 import android.content.SharedPreferences
 
+@Suppress("unused")
 fun SharedPreferences.liveData(key: String, default: Int) = SharedPreferenceIntLiveData(this, key, default)
 
+@Suppress("unused")
 fun SharedPreferences.liveData(key: String, default: String) = SharedPreferenceStringLiveData(this, key, default)
 
+@Suppress("unused")
 fun SharedPreferences.liveData(key: String) = SharedPreferenceStringNullableLiveData(this, key, null)
 
+@Suppress("unused")
 fun SharedPreferences.liveData(key: String, default: Boolean) = SharedPreferenceBooleanLiveData(this, key, default)
 
+@Suppress("unused")
 fun SharedPreferences.liveData(key: String, default: Float) = SharedPreferenceFloatLiveData(this, key, default)
 
+@Suppress("unused")
 fun SharedPreferences.liveData(key: String, default: Long) = SharedPreferenceLongLiveData(this, key, default)
 
+@Suppress("unused")
 fun SharedPreferences.liveData(key: String, default: Set<String>) = SharedPreferenceStringSetLiveData(this, key, default)
