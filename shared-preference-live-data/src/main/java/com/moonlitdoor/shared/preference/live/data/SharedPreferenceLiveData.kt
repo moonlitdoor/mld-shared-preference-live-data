258d74455dbeccb1219508785577526636359889
Brian Amesbury <bamesbury@sofi.org>
2018-10-06 22:09:59 -0600

    generate javadoc aar and sources aar

--- shared-preference-live-data/src/main/java/com/moonlitdoor/shared/preference/live/data/SharedPreferenceLiveData.kt
+++ shared-preference-live-data/src/main/java/com/moonlitdoor/shared/preference/live/data/SharedPreferenceLiveData.kt
@@ -3,6 +3,10 @@ package com.moonlitdoor.shared.preference.live.data
 import android.content.SharedPreferences
 import androidx.lifecycle.LiveData
 
+
+/**
+ * This class has no useful javadoc.
+ */
 abstract class SharedPreferenceLiveData<T>(val preferences: SharedPreferences, private val key: String, private val default: T) : LiveData<T>(), SharedPreferences.OnSharedPreferenceChangeListener {
 
   override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
